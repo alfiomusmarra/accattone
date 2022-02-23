71f0b170babdc5856da78080c360491d99f05ea9
alfiomusmarra <alfiomusmarra@gmail.com>
2022-02-23 21:13:04 +0100

    o

--- main.ts
+++ main.ts
@@ -4,8 +4,8 @@ let resto = 0
 let numero_binario = ""
 input.onButtonPressed(Button.A, function () {
     quoziente_intero = 1
+    valore_decimale_casuale = randint(0, 9)
     while (quoziente_intero > 0) {
-        valore_decimale_casuale = randint(0, 9)
         quoziente_intero = Math.idiv(valore_decimale_casuale, 2)
         resto = valore_decimale_casuale % 2
         numero_binario = "" + resto + numero_binario
