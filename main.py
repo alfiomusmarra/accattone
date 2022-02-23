719502bc34cebac143d2728e2730a844d8a8cf1a
alfiomusmarra <alfiomusmarra@gmail.com>
2022-02-23 21:07:16 +0100

    n

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-valore_decimale_casuale = 0
 quoziente_intero = 0
+valore_decimale_casuale = 0
 resto = 0
 numero_binario = ""
 
 def on_button_pressed_a():
-    global valore_decimale_casuale, quoziente_intero, resto, numero_binario
-    valore_decimale_casuale = randint(0, 9)
-    quoziente_intero = Math.idiv(valore_decimale_casuale, 2)
-    resto = valore_decimale_casuale % 2
-    numero_binario = str(resto)+numero_binario
+    global quoziente_intero, valore_decimale_casuale, resto, numero_binario
+    quoziente_intero = 1
+    while quoziente_intero>0:
+        valore_decimale_casuale = randint(0, 9)
+        quoziente_intero = Math.idiv(valore_decimale_casuale, 2)
+        resto = valore_decimale_casuale % 2
+        numero_binario = "" + str(resto) + numero_binario
+        valore_decimale_casuale = quoziente_intero
 input.on_button_pressed(Button.A, on_button_pressed_a)
--- main.ts
+++ main.ts
@@ -1,10 +1,15 @@
-let valore_decimale_casuale = 0
 let quoziente_intero = 0
+let valore_decimale_casuale = 0
 let resto = 0
 let numero_binario = ""
 input.onButtonPressed(Button.A, function () {
-    valore_decimale_casuale = randint(0, 9)
-    quoziente_intero = Math.idiv(valore_decimale_casuale, 2)
-    resto = valore_decimale_casuale % 2
-    numero_binario = "" + resto + numero_binario
+    quoziente_intero = 1
+    while (quoziente_intero > 0) {
+        valore_decimale_casuale = randint(0, 9)
+        quoziente_intero = Math.idiv(valore_decimale_casuale, 2)
+        resto = valore_decimale_casuale % 2
+        numero_binario = "" + resto + numero_binario
+        valore_decimale_casuale = quoziente_intero
+    }
+    basic.showString(numero_binario)
 })
