985b0706107b15044a29dfe40d2734865d8018fd
alfiomusmarra <alfiomusmarra@gmail.com>
2022-02-23 21:01:36 +0100

    Aggiorna pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,11 +1,10 @@
 let valore_decimale_casuale = 0
 let quoziente_intero = 0
 let resto = 0
-let numero_binario = 0
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+let numero_binario = ""
+input.onButtonPressed(Button.A, function () {
     valore_decimale_casuale = randint(0, 9)
     quoziente_intero = Math.idiv(valore_decimale_casuale, 2)
     resto = valore_decimale_casuale % 2
-    numero_binario = 0
+    numero_binario = "" + resto + numero_binario
 })
