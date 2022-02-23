6835a15e692b0249a0a2cefb61aee93012bc61cc
alfiomusmarra <alfiomusmarra@gmail.com>
2022-02-23 21:43:48 +0100

    Aggiorna main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-quoziente_intero = 0
-valore_decimale_casuale = 0
-resto = 0
-numero_binario = ""
-
 def on_button_pressed_a():
-    global quoziente_intero, valore_decimale_casuale, resto, numero_binario
-    quoziente_intero = 1
-    while quoziente_intero>0:
-        valore_decimale_casuale = randint(0, 9)
-        quoziente_intero = Math.idiv(valore_decimale_casuale, 2)
-        resto = valore_decimale_casuale % 2
-        numero_binario = "" + str(resto) + numero_binario
-        valore_decimale_casuale = quoziente_intero
+    basic.clear_screen()
 input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    if switch_mostra_nascondi_decimale==0:
+        pass
+    basic.show_string("" + str((valore_decimale_da_convertire)))
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+numero_binario = ""
+resto = 0
+valore_decimale_da_convertire = 0
+switch_mostra_nascondi_decimale = 0
+switch_mostra_nascondi_decimale = 0
+quoziente_intero = 1
+valore_decimale_casuale = randint(0, 9)
+valore_decimale_da_convertire = valore_decimale_casuale
+while quoziente_intero > 0:
+    quoziente_intero = Math.idiv(valore_decimale_casuale, 2)
+    resto = valore_decimale_casuale % 2
+    numero_binario = "" + str(resto) + numero_binario
+    valore_decimale_casuale = quoziente_intero
+basic.show_string("" + str((valore_decimale_da_convertire)))
--- main.ts
+++ main.ts
@@ -1,15 +1,27 @@
-let quoziente_intero = 0
-let valore_decimale_casuale = 0
-let resto = 0
-let numero_binario = ""
 input.onButtonPressed(Button.A, function () {
-    quoziente_intero = 1
-    valore_decimale_casuale = randint(0, 9)
-    while (quoziente_intero > 0) {
-        quoziente_intero = Math.idiv(valore_decimale_casuale, 2)
-        resto = valore_decimale_casuale % 2
-        numero_binario = "" + resto + numero_binario
-        valore_decimale_casuale = quoziente_intero
-    }
-    basic.showString(numero_binario)
+	
 })
+input.onButtonPressed(Button.B, function () {
+    if (switch_mostra_nascondi_decimale == 0) {
+        basic.clearScreen()
+        switch_mostra_nascondi_decimale = 1
+    } else {
+        basic.showString("" + (valore_decimale_da_convertire))
+        switch_mostra_nascondi_decimale = 0
+    }
+})
+let numero_binario = ""
+let resto = 0
+let valore_decimale_da_convertire = 0
+let switch_mostra_nascondi_decimale = 0
+switch_mostra_nascondi_decimale = 0
+let quoziente_intero = 1
+let valore_decimale_casuale = randint(0, 9)
+valore_decimale_da_convertire = valore_decimale_casuale
+while (quoziente_intero > 0) {
+    quoziente_intero = Math.idiv(valore_decimale_casuale, 2)
+    resto = valore_decimale_casuale % 2
+    numero_binario = "" + resto + numero_binario
+    valore_decimale_casuale = quoziente_intero
+}
+basic.showString("" + (valore_decimale_da_convertire))
